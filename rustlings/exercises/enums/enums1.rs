c7196388538ced84597bc35a43cb2f4231940de8
Chris Keefe <crk239@nau.edu>
2020-09-02 11:01:48 -0700

    NEW: adds completed enum rustlings

--- rustlings/exercises/enums/enums1.rs
+++ rustlings/exercises/enums/enums1.rs
@@ -1,11 +1,17 @@
 // enums1.rs
 // Make me compile! Execute `rustlings hint enums1` for hints!
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    // NOTE: it appears to be impossible to give these variants types, as
+    // Debug is implemented on enum _values_, not constructors. (These are apparently
+    // functions) under the hood: 
+    // https://stackoverflow.com/questions/53927598/debug-trait-not-implemented-for-some-enum-variants
+    Quit,
+    Echo,
+    Move,
+    ChangeColor
 }
 
 fn main() {
