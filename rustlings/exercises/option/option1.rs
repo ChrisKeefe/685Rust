682c28c70bfb6b88cfdab0433a2c5825a1efa543
Chris Keefe <crk239@nau.edu>
2020-09-16 16:01:19 -0700

    Adds Option rustlings

--- rustlings/exercises/option/option1.rs
+++ rustlings/exercises/option/option1.rs
@@ -5,19 +5,30 @@
 
 // you can modify anything EXCEPT for this function's sig
 fn print_number(maybe_number: Option<u16>) {
-    println!("printing: {}", maybe_number.unwrap());
+    if maybe_number.is_some(){
+        println!("printing: {}", maybe_number.unwrap());
+    }
 }
 
-fn main() {
-    print_number(13);
-    print_number(99);
+// I'm fundamentally not grasping the point of this exercise. Is there a reason
+// we would ever wrap integers in Some() like this for printing that I'm missing? 
 
+fn main() {
+    print_number(Some(13));
+    print_number(Some(99));
+
+
+// Does using an array of Option<u16>s just make it possible for us to initialize
+// the array to None so we can assign as needed later, checking manually for None?
+// Maybe this is an optimization strategy, so we can avoid Vec's overhead? 
+// And is this actually one exercise, or two?
     let mut numbers: [Option<u16>; 5];
+    numbers = [None, None, None, None, None];
     for iter in 0..5 {
         let number_to_add: u16 = {
             ((iter * 1235) + 2) / (4 * 16)
         };
 
-        numbers[iter as usize] = number_to_add;
+        numbers[iter as usize] = Some(number_to_add);
     }
 }
