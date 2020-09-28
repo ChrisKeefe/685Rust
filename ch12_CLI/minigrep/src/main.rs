8bfd5f1984de7e1cdd12080d79bdeee123e4a543
Chris Keefe <crk239@nau.edu>
2020-09-28 10:17:50 -0700

    Routes error messages to stderr, not stdout

--- ch12_CLI/minigrep/src/main.rs
+++ ch12_CLI/minigrep/src/main.rs
@@ -6,12 +6,12 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cfig = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     
     if let Err(e) = minigrep::run(cfig) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
