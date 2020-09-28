7fb6065ccbf4cd9778aa7cbb94c98657593cc660
Chris Keefe <crk239@nau.edu>
2020-09-27 22:31:32 -0700

    Refactor for error handling and separation of concerns

--- ch12_CLI/minigrep/src/main.rs
+++ ch12_CLI/minigrep/src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 struct Config {
     filename: String,
@@ -7,32 +9,43 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config{
+    fn new(args: &[String]) -> Result<Config, &'static str>{
         // NOTE: we disregard args[0], which holds the executable's filepath
-
+        // Here, we treat only args 1 and 2 as args for communication purposes
+        let argslen = args.len() - 1;
+        if argslen != 2{
+            return Err("Exactly two command-line arguments required.");
+        }
+        
+        
         let query = args[1].clone();
         let filename = args[2].clone();
         
-        Config {query, filename}
+        Ok(Config {query, filename})
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let cfig = Config::new(&args);
+    let cfig = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
     println!("Searching for \'{}\'", cfig.query);
-    
-    read_file(&cfig.filename);
-    // TODO: improve error handling
-    // TODO: consolidate all error handling in one place
-    let contents = fs::read_to_string(cfig.filename)
-    .expect("Something went wrong reading the file");
-    
-    println!("With text:\n{}", contents);
+    println!("In file \'{}\'\n", cfig.filename);
+
+    if let Err(e) = run(cfig) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
 }
 
+fn run(cfig: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(cfig.filename)?;
+    
+    println!("With text:\n{}", contents);
 
-fn read_file(filename: &str) -> String {
-    println!("In file \'{}\'\n", filename);
-    String::from("remove this")
+    Ok(())
 }
