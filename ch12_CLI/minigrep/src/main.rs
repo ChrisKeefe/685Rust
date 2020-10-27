144f449cb19be4f8ee3c78e20d670a65882029d9
Chris Keefe <crk239@nau.edu>
2020-10-26 22:04:19 -0700

    Replaces clone with iterators in minigrep

--- ch12_CLI/minigrep/src/main.rs
+++ ch12_CLI/minigrep/src/main.rs
@@ -4,8 +4,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let cfig = Config::new(&args).unwrap_or_else(|err| {
+    let cfig = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
