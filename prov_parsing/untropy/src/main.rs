ed9607a5d5f68e6aaafd6309ede1652b70e509d3
Chris Keefe <crk239@nau.edu>
2020-11-09 17:42:52 -0700

    Handles arg parsing errors more nicely

--- prov_parsing/untropy/src/main.rs
+++ prov_parsing/untropy/src/main.rs
@@ -7,25 +7,26 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         // For now, we'll take in one fp only, Later, a Vec<String>
-        // TODO: Use a Result here: Ch12.03
         if args.len() != 2 {
-            println!("Please provide exactly one fp argument");
-            process::exit(1);
+            return Err("Please provide exactly one fp argument");
         }
 
         // TODO: factor out clone for performance? (book Ch13)
         let fp = args[1].clone();
         println!("Storing fp {} in config.", fp);
 
-        Config { fp }
+        Ok(Config { fp })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Invalid arguments: {}", err);
+        process::exit(1);
+    });
 
     run(config);
     process::exit(0);
