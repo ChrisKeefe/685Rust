5157fa2eefcef18d2eaae8962d9554f0c9afeb75
Chris Keefe <crk239@nau.edu>
2020-11-09 18:13:35 -0700

    Factors body out into lib.rs

---
+++ prov_parsing/untropy/src/lib.rs
@@ -0,0 +1,26 @@
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Config {
+    pub fp: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        // For now, we'll take in one fp only, Later, a Vec<String>
+        if args.len() != 2 {
+            return Err("Please provide exactly one fp argument");
+        }
+
+        // TODO: factor out clone for performance? (book Ch13)
+        let fp = args[1].clone();
+        println!("Storing fp {} in config.", fp);
+
+        Ok(Config { fp })
+    }
+}
+
+pub fn run(conf: Config) -> Result<(), Box<dyn Error>> {
+    println!("Now we have a config {:?}", conf);
+    Ok(())
+}
--- prov_parsing/untropy/src/main.rs
+++ prov_parsing/untropy/src/main.rs
@@ -1,26 +1,6 @@
 use std::env;
-use std::error::Error;
 use std::process;
-
-#[derive(Debug)]
-struct Config {
-    fp: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        // For now, we'll take in one fp only, Later, a Vec<String>
-        if args.len() != 2 {
-            return Err("Please provide exactly one fp argument");
-        }
-
-        // TODO: factor out clone for performance? (book Ch13)
-        let fp = args[1].clone();
-        println!("Storing fp {} in config.", fp);
-
-        Ok(Config { fp })
-    }
-}
+use untropy::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -29,16 +9,10 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config){
+    if let Err(e) = untropy::run(config){
         println!("Runtime Error: {}", e);
         process::exit(1);
     };
-    
+
     process::exit(0);
-}
-
-
-fn run(conf: Config) -> Result<(), Box<dyn Error>> {
-    println!("Now we have a config {:?}", conf);
-    Ok(())
 }
